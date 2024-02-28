e7d7cd3dfbd98c9fe92857a054b2d107d581a988
Monish <mp051998@gmail.com>
2024-02-28 05:46:56 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import { configDotenv } from 'dotenv';
+import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+// Load the environment variables
+configDotenv();
+
+const app = express();
+
+// Check if this is a test environment and then set the port
+const isTest = process.env.NODE_ENV === 'test';
+
+// Set the port
+const port = process.env.PORT || 3000;
+
+// Allow JSON parsing
+app.use(express.json());
+
+// Register the routes
+
+// Set up swagger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Helpine App Backend API Documentation',
+      version: '1.0.0',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Development server',
+    },
+  ],
+  apis: ['./src/routes/*.ts'], // files containing the routes with spec comments
+};
+const openapiSpecification = swaggerJsdoc(swaggerOptions);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+
+// Start the server if this is not a test environment
+if (!isTest) {
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+}
+
+export { app };
